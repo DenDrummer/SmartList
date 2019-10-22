4a8dfb67fd5b376da0335ed1097d4ea3c93abc42
DenDrummer <muzikant96@gmail.com>
2019-10-22 15:08:27 +0200

    first draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('toggleOverlay', [
+      state('overlayShown', style({})),
+      state('overlayHidden', style({})),
+      transition('* => *', [animate('1s')])
+    ])
+  ]
 })
-export class AppComponent {
-  title = 'SmartList';
+export class AppComponent implements OnInit{
+  name = 'Listom';
+  showOverlay = false;
+  showSidebar = false;
+  windowWidth: number;
+
+  ngOnInit(): void {
+    this.windowWidth = window.innerWidth;
+  }
+
+  toggleOverlay() {
+    this.showOverlay = !this.showOverlay;
+  }
+
+  @HostListener('window:resize')
+  onResize(){
+    this.windowWidth = window.innerWidth;
+  }
 }
